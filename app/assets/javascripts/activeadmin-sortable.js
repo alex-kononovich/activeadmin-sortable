9f5510c84723247a416b646e11c63e5acfa8912c
Alex K <flskif@gmail.com>
2014-05-22 12:03:01 +0300

    Grab item's position from index to prevent errors

--- app/assets/javascripts/activeadmin-sortable.js
+++ app/assets/javascripts/activeadmin-sortable.js
@@ -10,24 +10,12 @@
     this.sortable({
       update: function(event, ui) {
         var url = ui.item.find('[data-sort-url]').data('sort-url');
-
-        var was_position = ui.item.find('[data-position]').data('position');
-
-        var next_position = ui.item.next().find('[data-position]').data('position');
-        var prev_position = ui.item.prev().find('[data-position]').data('position');
-
-        var target_position = ui.item.index() + 1;
-
-        if (next_position && was_position > next_position) {
-          target_position = next_position;
-        } else if (prev_position && was_position < prev_position) {
-          target_position = prev_position;
-        }
+        var position = ui.item.index() + 1;
 
         $.ajax({
           url: url,
           type: 'post',
-          data: { position: target_position },
+          data: { position: position },
           success: function() {
             // repaint table
             container.find('tr').removeClass('odd').removeClass('even');
